9006d5dc115c18561b1021039d60ea0bed815b74
AnirudhHanda <anirudhhanda3@gmail.com>
2024-09-06 21:33:18 +0530

    created a react frontend

---
+++ O205JobLe2RESTReact/src/main/java/com/walter/o205joble2restreact/O205JobLe2RestReactApplication.java
@@ -0,0 +1,14 @@
+package com.walter.o205joble2restreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O205JobLe2RestReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(O205JobLe2RestReactApplication.class, args);
+        System.out.println("Hello World!!");
+    }
+
+}
---
+++ O205JobLe2RESTReact/src/test/java/com/walter/o205joble2restreact/O205JobLe2RestReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walter.o205joble2restreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O205JobLe2RestReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
